51cc469e4d7c8c1d5ca6ed1f28f7b77098c1a18c
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2024-11-26 13:22:06 -0300

    Initial Commit

---
+++ LoginAnimation/LoginAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAnimationApp.swift
+//  LoginAnimation
+//
+//  Created by Marcelo Sampaio on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
